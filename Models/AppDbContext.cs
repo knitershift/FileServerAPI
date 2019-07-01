31b57a19bea03b169feecd82ae931a550a532f2f
Ярослав Ігорович <yaroslaw@live.ru>
2019-07-01 19:25:27 +0300

    Add project files.

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileServerAPI.Models
+{
+    public class AppDbContext: DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public AppDbContext(DbContextOptions options): base(options)
+        {
+        }
+    }
+}
