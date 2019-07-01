8e15bbccffa80b36d22fa2b2ea94850cdf00e59f
Ярослав Ігорович <yaroslaw@live.ru>
2019-07-01 20:13:35 +0300

    Add Authorize

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FileServerAPI.Controllers
@@ -12,6 +13,7 @@ namespace FileServerAPI.Controllers
     {
         // GET api/values
         [HttpGet]
+        [Authorize]
         public ActionResult<IEnumerable<string>> Get()
         {
             return new string[] { "value1", "value2" };
